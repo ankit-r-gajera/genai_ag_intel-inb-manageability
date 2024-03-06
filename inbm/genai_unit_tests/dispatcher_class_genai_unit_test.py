171e9daa19851db0e94911e0f746dc9553241e09
Ankit Gajera <107458035+ankit-r-gajera@users.noreply.github.com>
2024-03-06 17:44:12 +0530

    Delete inbm/genai_unit_tests/dispatcher_class_genai_unit_test.py

--- inbm/genai_unit_tests/dispatcher_class_genai_unit_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm-lib/')
-sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/')
-sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/dispatcher-agent/')
-sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/dispatcher-agent/dispatcher/')
-
-if __name__ == '__main__':
-    pytest.main()
