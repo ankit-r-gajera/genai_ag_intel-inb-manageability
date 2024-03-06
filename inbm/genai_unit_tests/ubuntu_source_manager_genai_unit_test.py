9752e326c29d7c852a8930ef9859213ca0cdca4d
Gen AI Copilot <141839948+nex-maximus@users.noreply.github.com>
2024-03-06 17:52:04 +0530

    updating inbm/genai_unit_tests/ubuntu_source_manager_genai_unit_test.py[skip ci]

---
+++ inbm/genai_unit_tests/ubuntu_source_manager_genai_unit_test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/')
+sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/dispatcher-agent/')
+sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/dispatcher-agent/dispatcher/')
+sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm/dispatcher-agent/dispatcher/source/')
+sys.path.append('/home/runner/GITHUB_ACTION_RUNNERS/_work/genai_ag_intel-inb-manageability/genai_ag_intel-inb-manageability/inbm-lib/')
+
+if __name__ == '__main__':
+    pytest.main()
